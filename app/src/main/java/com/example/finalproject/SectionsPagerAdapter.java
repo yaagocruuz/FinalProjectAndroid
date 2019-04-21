c8b3043429e5cb3d3edafae235a1aa3e8050ab9b
Yago Cruz <yaagocruuz@users.noreply.github.com>
2019-04-20 21:49:10 -0300

    Adding Tabs
    Added initial code for a "Tab Bar"

---
+++ app/src/main/java/com/example/finalproject/SectionsPagerAdapter.java
@@ -0,0 +1,46 @@
+package com.example.finalproject;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    public SectionsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new Tab1();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        //return the number of tabs
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "Tab 1";
+            case 1:
+                return "Tab 2";
+            case 2:
+                return "Tab 3";
+            default:
+                return null;
+        }
+    }
+}
